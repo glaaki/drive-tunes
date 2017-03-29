76ab55789d0fc9c8ebad8d6d032a6948e0be0717
glaaki <dreadpiratesean@gmail.com>
2017-03-29 16:06:25 -0500

    changed to use google sheets instead of keep

--- tunes.py
+++ tunes.py
@@ -1,27 +1,74 @@
 #!/usr/bin/env python
 
 
-from __future__ import unicode_literals
-from selenium import webdriver
+from __future__ import unicode_literals, print_function
+import httplib2
+import os
 import youtube_dl
 
+from apiclient import discovery
+from oauth2client import client
+from oauth2client import tools
+from oauth2client.file import Storage
 
-def make_video_list(user, pswd):
-    driver = webdriver.Chrome()
-    driver.implicitly_wait(10)
-    driver.get('https://keep.google.com/')
-    username_field = driver.find_element_by_id('Email')
-    username_field.send_keys(user)
-    next_button = driver.find_element_by_name('signIn')
-    next_button.click()
-    pw_field = driver.find_element_by_id('Passwd')
-    pw_field.send_keys(pswd)
-    
-    # sign in button is not 'visible' for some reason, just click it with vanilla js.
-    driver.execute_script("document.getElementById('signIn').click()")
+try:
+    import argparse
+    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+except ImportError:
+    flags = None
 
+# If modifying these scopes, delete your previously saved credentials
+# at ~/.credentials/sheets.googleapis.com-python-quickstart.json
+SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly'
+CLIENT_SECRET_FILE = 'client_secret.json'
+APPLICATION_NAME = 'Sheet Tunes'
+
+
+def get_credentials():
+    """Gets valid user credentials from storage.
+
+    If nothing has been stored, or if the stored credentials are invalid,
+    the OAuth2 flow is completed to obtain the new credentials.
+
+    Returns:
+        Credentials, the obtained credential.
+    """
+    home_dir = os.path.expanduser('~')
+    credential_dir = os.path.join(home_dir, '.credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir,
+                                   'sheets.googleapis.com-python-quickstart.json')
+
+    store = Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        flow.user_agent = APPLICATION_NAME
+        if flags:
+            credentials = tools.run_flow(flow, store, flags)
+        else: # Needed only for compatibility with Python 2.6
+            credentials = tools.run(flow, store)
+        print('Storing credentials to ' + credential_path)
+    return credentials
+
+
+def get_song_list(user, pswd):
+    credentials = get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    discoveryUrl = ('https://sheets.googleapis.com/$discovery/rest?'
+                    'version=v4')
+    service = discovery.build('sheets', 'v4', http=http,
+                              discoveryServiceUrl=discoveryUrl)
+    spreadsheetId = '1w0iR-v-SYYzpznMdEcA94i6MCIiN0VLUE59tGUajlBI'
+    rangeName = 'Song Data!A2:D'
+    result = service.spreadsheets().values().get(
+        spreadsheetId=spreadsheetId, range=rangeName).execute()
+    return result.get('values', [])
 
 def main():
+    songs = get_song_list()
+
     options = {
         'format': 'bestaudio/best',
         'extractaudio': True,
@@ -39,5 +86,4 @@ def main():
 
 
 if __name__ == "__main__":
-    #main()
-    make_video_list()
+    main()
