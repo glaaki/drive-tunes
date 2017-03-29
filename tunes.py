82a2c8fd46c7a1a37ae6a02e1f201db1601a850d
glaaki <dreadpiratesean@gmail.com>
2017-03-29 14:14:36 -0500

    first pass at selenium for google keep

--- tunes.py
+++ tunes.py
@@ -2,20 +2,42 @@
 
 
 from __future__ import unicode_literals
+from selenium import webdriver
 import youtube_dl
 
 
-options = {
-    'format': 'bestaudio/best',
-    'extractaudio': True,
-    'audioformat': "mp3",
-    'postprocessors': [{
-        'key': 'FFmpegExtractAudio',
-        'preferredcodec': 'mp3',
-        'preferredquality': '192',
-    }],
-    'outtmpl': '%(title)s.%(ext)s',
-    'noplaylist': True
-}
-with youtube_dl.YoutubeDL(options) as ydl:
-    ydl.download(['https://www.youtube.com/watch?v=dQw4w9WgXcQ'])
+def make_video_list(user, pswd):
+    driver = webdriver.Chrome()
+    driver.implicitly_wait(10)
+    driver.get('https://keep.google.com/')
+    username_field = driver.find_element_by_id('Email')
+    username_field.send_keys(user)
+    next_button = driver.find_element_by_name('signIn')
+    next_button.click()
+    pw_field = driver.find_element_by_id('Passwd')
+    pw_field.send_keys(pswd)
+    
+    # sign in button is not 'visible' for some reason, just click it with vanilla js.
+    driver.execute_script("document.getElementById('signIn').click()")
+
+
+def main():
+    options = {
+        'format': 'bestaudio/best',
+        'extractaudio': True,
+        'audioformat': "mp3",
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+        'outtmpl': '%(title)s.%(ext)s',
+        'noplaylist': True
+    }
+    with youtube_dl.YoutubeDL(options) as ydl:
+        ydl.download(['https://www.youtube.com/watch?v=dQw4w9WgXcQ'])
+
+
+if __name__ == "__main__":
+    #main()
+    make_video_list()
