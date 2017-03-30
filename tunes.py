185df8975bca04dc1a11c5b25671a54a65b6785c
glaaki <dreadpiratesean@gmail.com>
2017-03-30 14:33:42 -0500

    gave write permissions to sheet api

--- tunes.py
+++ tunes.py
@@ -19,7 +19,7 @@ except ImportError:
 
 # If modifying these scopes, delete your previously saved credentials
 # at ~/.credentials/sheets.googleapis.com-python-quickstart.json
-SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly'
+SCOPES = 'https://www.googleapis.com/auth/spreadsheets'
 CLIENT_SECRET_FILE = 'client_secret.json'
 APPLICATION_NAME = 'Drive Tunes'
 LOCAL_SAVE_PATH = 'Music'
